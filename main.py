ff92c7e5220f4bc264c838f7cfdf9c7d7c293623
Christian Martín Díaz <146923531+chrismartindiaz@users.noreply.github.com>
2024-02-28 14:41:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,13 @@ def translate_role_for_streamlit(user_role):
 # Inicializamos el chat en caso de que no se haya iniciado
 if "chat_session" not in st.session_state:
     st.session_state.chat_session = model.start_chat(history=[])
-
+    
+    # Enviamos el mensaje de presentación al iniciar el chat
+    intro_message = ("Preséntate como 'BeatBuddy' un chatbot muy interactivo que se encarga de recomendar canciones "
+                     "relacionadas con artistas, géneros, décadas músicales, estados de ánimo y preguntas musicales, "
+                     "en caso de que se te realice cualquier otra pregunta no responderás."
+                     "Además, no se usará bajo ningún concepto caracteres en negrita y en cursiva, esto es muy importante.")
+    st.session_state.chat_session.send_message(intro_message)
 # Streamlit
 with st.sidebar:
     audio = audiorecorder("Click to send voice message", "Recording... Click when you're done", key="recorder")
