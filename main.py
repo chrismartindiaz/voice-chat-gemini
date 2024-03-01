267d67690e12cb4d5c04db879f2ee4f0cc208128
Christian Martín Díaz <146923531+chrismartindiaz@users.noreply.github.com>
2024-03-01 12:25:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,10 @@ def translate_role_for_streamlit(user_role):
         return user_role
 
 # Inicializamos el chat en caso de que no se haya iniciado
-if "chat_session" not in st.session_state:    
+if "chat_session" not in st.session_state:
+
+    st.session_state.chat_session = model.start_chat(history=[])
+
     # Enviamos el mensaje de presentación al iniciar el chat
     intro_message = ("Preséntate como 'BeatBuddy' un chatbot muy interactivo que se encarga de recomendar canciones "
                      "relacionadas con artistas, géneros, décadas músicales, estados de ánimo y preguntas musicales, "
